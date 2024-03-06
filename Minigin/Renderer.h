8f0ffb62136307709ea03fd9a44465ca5657e21f
Vik Praet <vik.praet@gmail.com>
2024-03-06 20:13:46 +0100

    added imgui_plot

--- Minigin/Renderer.h
+++ Minigin/Renderer.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <memory>
 #include <SDL.h>
+
 #include "Singleton.h"
 
 namespace dae
@@ -12,7 +14,8 @@ namespace dae
 	{
 		SDL_Renderer* m_renderer{};
 		SDL_Window* m_window{};
-		SDL_Color m_clearColor{};	
+		SDL_Color m_clearColor{};
+
 	public:
 		void Init(SDL_Window* window);
 		void Render() const;
